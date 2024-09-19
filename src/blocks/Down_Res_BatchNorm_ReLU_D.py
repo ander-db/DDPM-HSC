263d249144b1c13c1002fce8d336c00d77ca62c1
Ander D <ander_dorado@hotmail.com>
2024-09-19 23:31:57 +0200

    chore: Remove unused code and file

--- src/blocks/Down_Res_BatchNorm_ReLU_D.py
+++ src/blocks/Down_Res_BatchNorm_ReLU_D.py
@@ -3,24 +3,6 @@ import torch.nn as nn
 from Res_GroupNorm_SiLU_D import ResBlockGroupNorm
 
 
-class Encoder_Down_Res_GroupNorm_SiLU_D(nn.Module):
-    def __init__(self, channel_layers: list[int] = [64, 128, 256, 512]):
-        super().__init__()
-        self.channel_layers = channel_layers
-        self.encoder = self._build_encoder()
-
-    def _build_encoder(self):
-        encoder = nn.ModuleList()
-        for i in range(len(self.channel_layers) - 1):
-            encoder.append(
-                Down_Res_GroupNorm_SiLU_D(
-                    self.channel_layers[i],
-                    self.channel_layers[i + 1],
-                )
-            )
-        return encoder
-
-
 class Down_Res_GroupNorm_SiLU_D(nn.Module):
     def __init__(self, in_channels, out_channels, *args, **kwargs):
         self.module = nn.Sequential(
@@ -31,4 +13,3 @@ class Down_Res_GroupNorm_SiLU_D(nn.Module):
 
     def forward(self, x):
         return self.module(x)
-
