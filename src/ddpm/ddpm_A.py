3b1e6e03a6a409f7bfdc2f7244abdc70348d1efc
Ander D <ander_dorado@hotmail.com>
2024-09-25 23:19:45 +0200

    updated ddpm

--- src/ddpm/ddpm_A.py
+++ src/ddpm/ddpm_A.py
@@ -5,18 +5,17 @@ import lightning as l
 
 from diffusers.schedulers.scheduling_ddpm import DDPMScheduler
 
-
-from typing import List
+from typing import List, Tuple
 from src.unet.UNet_Res_GroupNorm_SiLU_D import UNet_Res_GroupNorm_SiLU_D
 from src.blocks.PositionalEncoding import PositionalEncoding
 
 
-class DDPM_A(l.LightningModule):
+class DDPM_2D(l.LightningModule):
     def __init__(
         self,
         in_channels: int = 1,
         out_channels: int = 1,
-        diffusion_steps: int = 1000,
+        diffusion_steps: int = 50,
         pe_emb_dim=64,
         lr: float = 1e-3,
         dropout: float = 0.05,
@@ -24,9 +23,10 @@ class DDPM_A(l.LightningModule):
         beta_start: float = 0.002,
         beta_end: float = 0.2,
         encoder_channels: List[int] = [64, 128, 256, 512],
+        loss_fn: nn.Module = nn.MSELoss(),
         scheduler: str = "squaredcos_cap_v2",
     ):
-        super(DDPM_A, self).__init__()
+        super(DDPM_2D, self).__init__()
 
         self.save_hyperparameters()
         self.diffusion_steps = diffusion_steps
@@ -39,6 +39,7 @@ class DDPM_A(l.LightningModule):
         self.out_channels = out_channels
         self.encoder_channels = encoder_channels
         self.decoder_channels = encoder_channels[::-1]
+        self.loss = loss_fn
 
         self.time_embedding = PositionalEncoding(pe_emb_dim, diffusion_steps)
         self.noise_scheduler = DDPMScheduler(
@@ -47,6 +48,7 @@ class DDPM_A(l.LightningModule):
 
         self._build_network()
 
+    @torch.no_grad()
     def _build_network(self):
         self.unet = UNet_Res_GroupNorm_SiLU_D(
             in_channels=self.in_channels,
@@ -55,8 +57,7 @@ class DDPM_A(l.LightningModule):
             loss_fn=nn.MSELoss(),
         )
 
-
-    def forward(self, x, t):
+    def forward(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
         with torch.no_grad():
             pe_emb = self.time_embedding(t.detach())
 
@@ -65,3 +66,118 @@ class DDPM_A(l.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr, eps=1e-4)
         return optimizer
+
+    def training_step(self, batch, batch_idx):
+        ref, x = batch
+
+        batch_size = x.shape[0]
+
+        t = self._generate_random_timesteps(batch_size)
+
+        noisy_batch, noise = self._add_noise(x, t)
+        noisy_ref_concat = torch.cat(
+            [noisy_batch, ref], dim=1
+        )  # Concatenate the reference image with the noisy image to use it as guidance
+
+        noise_prediction = self.forward(noisy_ref_concat, t)
+        loss = self.loss(noise_prediction, noise).mean()
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        """
+        Compute the validation loss for the model by sampling the images and computing the loss
+
+        Args:
+            batch (Tuple[torch.Tensor, torch.Tensor]): Tuple containing the reference and input images
+            batch_idx (int): Batch index
+
+        Returns:
+            torch.Tensor: Validation loss
+        """
+        ref, x = batch
+        predicted_samples = self.sample(ref)
+
+        loss = self.loss(predicted_samples, x).mean()
+
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        """
+        Compute the test loss for the model by sampling the images and computing the loss
+
+        Args:
+            batch (Tuple[torch.Tensor, torch.Tensor]): Tuple containing the reference and input images
+            batch_idx (int): Batch index
+
+        Returns:
+            torch.Tensor: Test loss
+        """
+        ref, x = batch
+        predicted_samples = self.sample(ref)
+
+        loss = self.loss(predicted_samples, x).mean()
+
+        return loss
+
+    @torch.no_grad()
+    def add_noise(
+        self, x: torch.FloatTensor, t: torch.IntTensor
+    ) -> Tuple[torch.FloatTensor, torch.FloatTensor]:
+        """
+        Add noise to the input image x at timestep t
+
+        Args:
+            x (torch.FloatTensor): Input image
+            t (torch.IntTensor): Timestep
+
+        Returns:
+            Tuple[torch.FloatTensor, torch.FloatTensor]: Noisy image and noise tensor
+
+        """
+        noise = torch.randn_like(x).to(x.dtype)  # Ensure noise matches x's dtype
+
+        if not isinstance(noise, torch.FloatTensor):
+            raise ValueError("Noise must be a float tensor")
+
+        noise_img = self.noise_scheduler.add_noise(x, noise, t)
+        return noise_img, noise
+
+    @torch.no_grad()
+    def _generate_random_timesteps(self, batch_size: int) -> torch.Tensor:
+        return torch.randint(0, self.diffusion_steps, (batch_size,))
+
+    @torch.no_grad()
+    def sample(self, ref: torch.Tensor) -> torch.FloatTensor:
+        """
+        Sample from the model
+
+        Args:
+            x (torch.FloatTensor): Input image batch
+            ref (torch.FloatTensor): Reference image batch
+
+        Returns:
+            torch.FloatTensor: Sampled image
+        """
+
+        device = self.device
+        x = torch.randn_like(ref, device=device)
+        self.noise_scheduler.set_timesteps(self.diffusion_steps, device=device)
+
+        # Repite for each timestep
+        for i in range(self.diffusion_steps - 1, -1, -1):
+            model_input = torch.cat([x, ref], dim=1)
+            t = torch.full((ref.shape[0],), i, device=device, dtype=torch.long)
+            predicted_noise = self.forward(model_input, t)
+
+            x = self.noise_scheduler.step(
+                model_output=predicted_noise,
+                timestep=i,
+                sample=x[:, :1],
+                return_dict=False,
+            )[0]
+
+        if not isinstance(x, torch.FloatTensor):
+            raise ValueError("Output must be a float tensor")
+
+        return x
